d50d603512168f1da9da89e12ad6a00ec471cea5
DIMIK <dimik2214@gmail.com>
2023-02-07 21:56:22 +0100

    Initial commit

---
+++ src/main/java/com/dimik/dimikspring/DimikspringApplication.java
@@ -0,0 +1,13 @@
+package com.dimik.dimikspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimikspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DimikspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dimik/dimikspring/DimikspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimik.dimikspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DimikspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
